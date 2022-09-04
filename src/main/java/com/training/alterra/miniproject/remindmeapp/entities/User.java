918f693020063785651c07f827064a0988a53de4
nan <net.nandang@gmail.com>
2022-09-04 14:31:45 +0700

    update/-- update user entity

--- src/main/java/com/training/alterra/miniproject/remindmeapp/entities/User.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/entities/User.java
@@ -2,8 +2,12 @@ package com.training.alterra.miniproject.remindmeapp.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity(name = "User")
 @Table(name = "users")
@@ -14,4 +18,23 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private String fullName;
+
+    @Column(nullable = false, updatable = false)
+    private String email;
+
+    @Column(nullable = false, updatable = false)
+    private String password;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
 }
