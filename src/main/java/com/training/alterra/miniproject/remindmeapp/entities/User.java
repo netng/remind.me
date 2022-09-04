4c405f9945e4786fa9b2eec93da1c10638d7ef28
nan <net.nandang@gmail.com>
2022-09-04 14:52:42 +0700

    update/-- change antotation @AuditingListeners property, add relation to task entity (user has_many tasks)

--- src/main/java/com/training/alterra/miniproject/remindmeapp/entities/User.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/entities/User.java
@@ -5,13 +5,16 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity(name = "User")
 @Table(name = "users")
-@EntityListeners(AutoCloseable.class)
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public class User {
@@ -37,4 +40,11 @@ public class User {
     @Temporal(TemporalType.TIMESTAMP)
     @LastModifiedDate
     private Date updatedAt;
+
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Task> tasks = new ArrayList<>();
 }
