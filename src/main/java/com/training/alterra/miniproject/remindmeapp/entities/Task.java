613f044d2d04f2d02b707f3f82c41e5342fad68a
nan <net.nandang@gmail.com>
2022-09-04 14:51:39 +0700

    create/-- create entity task and its relation to user entity (task belongs_to user)

---
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/entities/Task.java
@@ -0,0 +1,38 @@
+package com.training.alterra.miniproject.remindmeapp.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Table(name = "tasks")
+@Entity(name = "Task")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class Task {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
