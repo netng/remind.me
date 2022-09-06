617aecad17e9427412f19e0a8597778cb4d2f91c
netng <net.nandang@gmail.com>
2022-09-06 15:05:00 +0700

    update/-- add createNewUser and listAllUsers route to UsersController

--- src/main/java/com/training/alterra/miniproject/remindmeapp/controllers/UsersController.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/controllers/UsersController.java
@@ -1,16 +1,34 @@
 package com.training.alterra.miniproject.remindmeapp.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.training.alterra.miniproject.remindmeapp.dtos.users.UserRequestDTO;
+import com.training.alterra.miniproject.remindmeapp.dtos.users.UserResponseDTO;
+import com.training.alterra.miniproject.remindmeapp.entities.User;
+import com.training.alterra.miniproject.remindmeapp.services.users.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UsersController {
 
-    @GetMapping("/users/{name}")
-    public void index(@PathVariable(name = "name") String name) {
-        System.out.println("Hello " + name);
+    @Autowired
+    IUserService userService;
+
+    @PostMapping("/users/signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<UserResponseDTO> createNewUser(@RequestBody UserRequestDTO userRequestDTO) {
+        UserResponseDTO userResponseDTO = userService.createNewUser(userRequestDTO);
+        return ResponseEntity.ok().body(userResponseDTO);
+    }
+
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<UserResponseDTO>> listAllUsers() {
+        List<UserResponseDTO> users = userService.listAllUsers();
+        return ResponseEntity.ok().body(users);
     }
 }
