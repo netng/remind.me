87c6d99cd537e9de66c48bc8cd6fa6100e97e6d9
nan <net.nandang@gmail.com>
2022-09-06 19:59:04 +0700

    update/-- add equalsAndHasCode equality

--- src/main/java/com/training/alterra/miniproject/remindmeapp/dtos/users/UserResponseDTO.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/dtos/users/UserResponseDTO.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+@EqualsAndHashCode
 public class UserResponseDTO implements Serializable {
     private Long id;
 
