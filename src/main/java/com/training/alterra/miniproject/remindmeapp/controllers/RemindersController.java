dd452e69afba6e73ba92788cdd9a7470acf8a03a
netng <net.nandang@gmail.com>
2022-09-07 12:13:32 +0700

    update/-- add listAllReminders route to RemindersController

--- src/main/java/com/training/alterra/miniproject/remindmeapp/controllers/RemindersController.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/controllers/RemindersController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
 public class RemindersController {
@@ -20,5 +22,12 @@ public class RemindersController {
     public ResponseEntity<ReminderResponseDTO> createNewReminder(@PathVariable Long userId, @RequestBody ReminderRequestDTO requestDTO) {
         ReminderResponseDTO responseDTO = reminderService.createNewReminder(userId, requestDTO);
         return ResponseEntity.ok().body(responseDTO);
+    }
+
+    @GetMapping("/users/{userId}/reminders")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<List<ReminderResponseDTO>> listAllReminders(@PathVariable Long userId) {
+        List<ReminderResponseDTO> reminderResponseDTOS = reminderService.listAllReminders(userId);
+        return ResponseEntity.ok().body(reminderResponseDTOS);
     }
 }
