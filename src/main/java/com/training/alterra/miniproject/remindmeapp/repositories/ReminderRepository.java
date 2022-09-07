4b8cf52ed6be5b67dc05e1d80d11ec37427826f9
netng <net.nandang@gmail.com>
2022-09-07 12:13:09 +0700

    update/-- add custom jpa findByUserId

--- src/main/java/com/training/alterra/miniproject/remindmeapp/repositories/ReminderRepository.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/repositories/ReminderRepository.java
@@ -4,6 +4,9 @@ import com.training.alterra.miniproject.remindmeapp.entities.Reminder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReminderRepository extends JpaRepository<Reminder, Long> {
+    List<Reminder> findByUserId(Long userId);
 }
