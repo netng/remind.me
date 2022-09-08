8c8341fdc062554a1b3cd44745732660a48c0e30
netng <net.nandang@gmail.com>
2022-09-08 12:00:05 +0700

    update/-- adjustment on timestamp reminderDateTime

--- src/main/java/com/training/alterra/miniproject/remindmeapp/entities/Schedule.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/entities/Schedule.java
@@ -20,7 +20,7 @@ public class Schedule {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime reminderDateTime;
 
     @Column(nullable = false, updatable = false)
