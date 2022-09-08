27bece648407316dfc8b220fbf2f8e4b967ac97a
netng <net.nandang@gmail.com>
2022-09-08 11:18:37 +0700

    update/-- add abstract method createNewSchedule

--- src/main/java/com/training/alterra/miniproject/remindmeapp/services/schedules/IScheduleService.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/services/schedules/IScheduleService.java
@@ -1,4 +1,9 @@
 package com.training.alterra.miniproject.remindmeapp.services.schedules;
 
+
+import com.training.alterra.miniproject.remindmeapp.dtos.schedules.ScheduleRequestDTO;
+import com.training.alterra.miniproject.remindmeapp.dtos.schedules.ScheduleResponseDTO;
+
 public interface IScheduleService {
+    ScheduleResponseDTO createNewSchedule(Long reminderId, ScheduleRequestDTO requestDTO);
 }
