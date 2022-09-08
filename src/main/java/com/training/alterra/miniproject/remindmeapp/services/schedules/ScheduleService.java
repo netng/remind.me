04cd1f2ccb420151501b4fb8b12c8a784199b20c
netng <net.nandang@gmail.com>
2022-09-08 14:45:43 +0700

    updat/-- modify createNewSchedule method with quartz scheduler code

--- src/main/java/com/training/alterra/miniproject/remindmeapp/services/schedules/ScheduleService.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/services/schedules/ScheduleService.java
@@ -2,14 +2,22 @@ package com.training.alterra.miniproject.remindmeapp.services.schedules;
 
 import com.training.alterra.miniproject.remindmeapp.dtos.schedules.ScheduleRequestDTO;
 import com.training.alterra.miniproject.remindmeapp.dtos.schedules.ScheduleResponseDTO;
+import com.training.alterra.miniproject.remindmeapp.entities.Reminder;
 import com.training.alterra.miniproject.remindmeapp.entities.Schedule;
+import com.training.alterra.miniproject.remindmeapp.exceptions.InvalidReminderDateTimeException;
+import com.training.alterra.miniproject.remindmeapp.jobs.ReminderJob;
 import com.training.alterra.miniproject.remindmeapp.repositories.ReminderRepository;
 import com.training.alterra.miniproject.remindmeapp.repositories.ScheduleRepository;
 import org.modelmapper.ModelMapper;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ScheduleService implements IScheduleService {
@@ -23,14 +31,36 @@ public class ScheduleService implements IScheduleService {
     @Autowired
     ModelMapper modelMapper;
 
+    @Autowired
+    private Scheduler scheduler;
+
     @Override
     public ScheduleResponseDTO createNewSchedule(Long reminderId, ScheduleRequestDTO requestDTO) {
-        Optional<Schedule> schedule = reminderRepository.findById(reminderId)
-                .map(reminder -> {
-                    requestDTO.setReminder(reminder);
-                    return scheduleRepository.save(convertToEntity(requestDTO));
-                });
-        return convertToDto(schedule.get());
+
+        ZoneId reminderDateTime = reminderRepository.findById(reminderId).get().getUser().getTimeZone();
+        Reminder reminderOfSchedule = reminderRepository.findById(reminderId).get();
+
+        try {
+            ZonedDateTime dateTime = ZonedDateTime.of(requestDTO.getReminderDateTime(), reminderDateTime);
+            if (dateTime.isBefore(ZonedDateTime.now())) {
+                throw new InvalidReminderDateTimeException(dateTime);
+            }
+
+            JobDetail jobDetail = buildJobDetail(reminderOfSchedule);
+            Trigger trigger = buildJobTrigger((jobDetail), dateTime);
+            scheduler.scheduleJob(jobDetail, trigger);
+
+            Optional<Schedule> schedule = reminderRepository.findById(reminderId)
+                    .map(reminder -> {
+                        requestDTO.setReminder(reminder);
+                        return scheduleRepository.save(convertToEntity(requestDTO));
+                    });
+
+            return convertToDto(schedule.get());
+
+        } catch (SchedulerException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Schedule convertToEntity(ScheduleRequestDTO requestDTO) {
@@ -39,5 +69,31 @@ public class ScheduleService implements IScheduleService {
 
     private ScheduleResponseDTO convertToDto(Schedule schedule) {
         return modelMapper.map(schedule, ScheduleResponseDTO.class);
+    }
+
+
+    private JobDetail buildJobDetail(Reminder reminder) {
+        JobDataMap jobDataMap = new JobDataMap();
+
+        jobDataMap.put("reminderUser", reminder.getUser().getFullName());
+        jobDataMap.put("reminderTitle", reminder.getTitle());
+        jobDataMap.put("reminderDescription", reminder.getDescription());
+
+        return JobBuilder.newJob(ReminderJob.class)
+                .withIdentity(UUID.randomUUID().toString(), "email-jobs")
+                .withDescription("Send Email Job")
+                .usingJobData(jobDataMap)
+                .storeDurably()
+                .build();
+    }
+
+    private Trigger buildJobTrigger(JobDetail jobDetail, ZonedDateTime startAt) {
+        return TriggerBuilder.newTrigger()
+                .forJob(jobDetail)
+                .withIdentity(jobDetail.getKey().getName(), "email-triggers")
+                .withDescription("Send Email Trigger")
+                .startAt(Date.from(startAt.toInstant()))
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withMisfireHandlingInstructionFireNow())
+                .build();
     }
 }
