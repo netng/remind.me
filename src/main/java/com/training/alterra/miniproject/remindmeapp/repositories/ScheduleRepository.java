8deeb87028ad66403fb40d1dba9dcf9dd7c4ba7f
netng <net.nandang@gmail.com>
2022-09-08 15:28:49 +0700

    update/-- add custom jpa findByReminderId

--- src/main/java/com/training/alterra/miniproject/remindmeapp/repositories/ScheduleRepository.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/repositories/ScheduleRepository.java
@@ -4,6 +4,9 @@ import com.training.alterra.miniproject.remindmeapp.entities.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findByReminderId(Long reminderId);
 }
