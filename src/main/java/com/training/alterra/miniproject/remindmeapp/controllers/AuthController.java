ae1290b31f06b3e8f9916f6284202213280b4881
netng <net.nandang@gmail.com>
2022-10-14 14:28:00 +0700

    add principal details to jwt response dto

--- src/main/java/com/training/alterra/miniproject/remindmeapp/controllers/AuthController.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/controllers/AuthController.java
@@ -25,10 +25,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -36,6 +33,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
@@ -74,7 +72,7 @@ public class AuthController {
         return ResponseEntity.ok(new ApiResponse(
                 "OK",
                 "success",
-                new JwtAuthenticationResponse(jwt))
+                new JwtAuthenticationResponse(jwt, authentication.getPrincipal()))
         );
     }
 
--- src/main/java/com/training/alterra/miniproject/remindmeapp/payloads/JwtAuthenticationResponse.java
+++ src/main/java/com/training/alterra/miniproject/remindmeapp/payloads/JwtAuthenticationResponse.java
@@ -6,16 +6,20 @@
  */
 package com.training.alterra.miniproject.remindmeapp.payloads;
 
+import com.training.alterra.miniproject.remindmeapp.entities.User;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class JwtAuthenticationResponse {
+public class JwtAuthenticationResponse<T> {
     private String accessToken;
     private String tokenType = "Bearer";
+    private T user;
 
-    public JwtAuthenticationResponse(String accessToken) {
+    public JwtAuthenticationResponse(String accessToken, T user) {
         this.accessToken = accessToken;
+        this.user = user;
+
     }
 }
